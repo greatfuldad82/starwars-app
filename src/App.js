80b37851aebb5aca13651f6ccfb566698da2385b
alexander mitchell <greatful_dad@icloud.com>
2021-09-09 14:34:24 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import StarWarsList from "./components/StarWarsList";
+
+function App() {
+  return (
+    <main className="my-3">
+    <Container>
+      <StarWarsList />
+    </Container>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/StarWarsList.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Spinner } from 'react-bootstrap';
+import axios from 'axios';
+import StarWars from './StarWars';
+
+
+const StarWarsList = () => {
+        const [people, setPeople] = useState([]);
+        const [loading, setLoading] = useState(true)
+
+        const fetchPeople = async () => {
+            const response = await axios.get("https://swapi.dev/api/people/");
+
+            const data = await response.data
+            setPeople(data);
+            setLoading(false)
+        };
+
+        useEffect(() => {
+            fetchPeople()
+        }, [])
+
+        console.log(people);
+
+
+        return (
+            <>
+                <h1 className="text-center">Star Wars List</h1>
+                {loading && (
+                    <Spinner animation="border" role="status" style={{
+                        display: "block", 
+                        margin: "auto", 
+                        height: "100px, width:100px",
+                        }}>
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>
+                )}
+                <Row>
+                    {!loading &&
+                     people.results.map((person, index) => (
+                        <Col key={index} sm={12} md={6}  lg={4} xl={3}>
+                        <StarWars person={person} />
+                        </Col>
+        ))}
+                    
+                </Row>
+                
+                </>
+    );
+};
+
+export default StarWarsList
